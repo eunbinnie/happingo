8b79060650b2374efa723a4437490587fa74f620
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-11-25 16:32:23 +0900

    feat: 헤더 레이아웃 및 테마 전환 기능 구현 (#4)
    * feat: 모바일 환경에 맞게 레이아웃 세팅
    
    * feat: 헤더 퍼블리싱 및 폰트 사이즈 2xs 추가
    
    * feat: svgr 추가 및 로고 svg로 변경
    
    * feat: 헤더에 연도, 월 동적 표기 추가
    
    * refactor: 헤더 로고 svg 파일 fill currentColor로 변경
    
    * feat: 현재 테마에 맞는 라이트/다크 아이콘 조건부 렌더링
    
    * style: 다크 모드 텍스트 투명도 조정 및 nav flex-wrap 적용
    
    * refactor: system 테마일 경우 라이트 아이콘으로 설정

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+import Logo from '@/assets/icons/logo.svg';
+
+import HeaderNav from './HeaderNav';
+
+const Header = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const monthLabel = String(month).padStart(2, '0'); // 월 01, 02, 03 형식으로 표시
+
+  return (
+    <header className="mt-5 px-3">
+      <div className="text-2xs dark:text-text/60 flex items-center justify-between font-light sm:text-xs">
+        <span>{year}</span>
+        <p>
+          매달 나만의 행복 미션 빙고 ㅡ{' '}
+          <strong className="font-medium">해핑고</strong>
+        </p>
+        <span>{monthLabel}</span>
+      </div>
+      <div className="mt-2">
+        <Link href="/" aria-label="해핑고 홈">
+          <Logo className="text-text" />
+        </Link>
+      </div>
+      <HeaderNav />
+    </header>
+  );
+};
+
+export default Header;
