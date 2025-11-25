2d7adb07fddba06041bef42c4513bd42d2af72d9
eunbinnie <dmsqlsdl54@gmail.com>
2025-11-25 15:39:39 +0900

    refactor: 헤더 로고 svg 파일 fill currentColor로 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
       </div>
       <div className="mt-2">
         <Link href="/" aria-label="해핑고 홈">
-          <Logo />
+          <Logo className="text-text" />
         </Link>
       </div>
     </header>
