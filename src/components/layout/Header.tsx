e41561543f4cf94914b473e16a9ee75c16f434b3
eunbinnie <dmsqlsdl54@gmail.com>
2025-11-25 15:22:20 +0900

    feat: 헤더에 연도, 월 동적 표기 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,17 +3,22 @@ import Link from 'next/link';
 import Logo from '@/assets/icons/logo.svg';
 
 const Header = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const monthLabel = String(month).padStart(2, '0'); // 월 01, 02, 03 형식으로 표시
+
   return (
     <header className="mt-5 px-3">
-      <div className="text-2xs flex items-center justify-between">
-        <span>2025</span>
+      <div className="text-2xs flex items-center justify-between sm:text-xs">
+        <span>{year}</span>
         <p>
           매달 나만의 행복 미션 빙고 ㅡ <strong>해핑고</strong>
         </p>
-        <span>11</span>
+        <span>{monthLabel}</span>
       </div>
       <div className="mt-2">
-        <Link href="/">
+        <Link href="/" aria-label="해핑고 홈">
           <Logo />
         </Link>
       </div>
