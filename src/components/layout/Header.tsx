77bf74a99c33fbb3b1de60eac55ac73ca801f499
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-11-26 15:53:39 +0900

    feat: 빙고 카드 영역 추가 및 레이아웃 수정 (#5)
    * fix: 헤더 margin-top 제거로 불필요한 스크롤 발생 수정
    
    * feat: 빙고 카드 영역 퍼블리싱
    
    * style: 헤더 영역 레이아웃 수정
    
    * style: 전체 여백 수정 및 레이아웃 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,14 +11,15 @@ const Header = () => {
   const monthLabel = String(month).padStart(2, '0'); // 월 01, 02, 03 형식으로 표시
 
   return (
-    <header className="mt-5 px-3">
-      <div className="text-2xs dark:text-text/60 flex items-center justify-between font-light sm:text-xs">
-        <span>{year}</span>
-        <p>
+    <header className="px-3">
+      <div className="text-2xs dark:text-text/60 flex flex-col font-light sm:text-xs">
+        <span>
+          {year} · {monthLabel}
+        </span>
+        <p className="text-center">
           매달 나만의 행복 미션 빙고 ㅡ{' '}
           <strong className="font-medium">해핑고</strong>
         </p>
-        <span>{monthLabel}</span>
       </div>
       <div className="mt-2">
         <Link href="/" aria-label="해핑고 홈">
--- src/components/layout/HeaderNav.tsx
+++ src/components/layout/HeaderNav.tsx
@@ -6,15 +6,13 @@ const Separator = () => {
 
 const HeaderNav = () => {
   return (
-    <nav className="dark:text-text/60 mt-8 flex flex-wrap items-center justify-between gap-x-2 text-xs sm:text-sm">
-      <div className="flex items-center gap-1">
-        <span>0 BINGO</span>
-        <Separator />
+    <nav className="dark:text-text/60 mt-4 flex flex-wrap items-center justify-between gap-x-2 text-xs sm:mt-7 sm:text-sm">
+      <div className="flex items-center gap-1.5">
         <button type="button">지난 빙고</button>
         <Separator />
         <button type="button">이미지 저장</button>
       </div>
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1.5">
         <button type="button">해핑고 사용법</button>
         <Separator />
         <button type="button">빙고 편집</button>
