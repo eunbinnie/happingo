71a9c44ffe1aa6298b45f4953a87633773a8fdb2
eunbinnie <dmsqlsdl54@gmail.com>
2025-11-26 15:31:56 +0900

    style: 전체 여백 수정 및 레이아웃 변경

--- src/components/bingo/BingoBoard.tsx
+++ src/components/bingo/BingoBoard.tsx
@@ -4,20 +4,25 @@ const BingoBoard = () => {
   const array = new Array(9).fill('빙고');
 
   return (
-    <div className="mt-8 grid w-full grid-cols-3 grid-rows-3 gap-2">
-      {array.map((item, index) => (
-        <div
-          key={index}
-          className={cn(
-            'flex aspect-3/4 items-center justify-center px-2 text-center text-xs font-medium break-keep sm:text-sm',
-            'border-text/25 bg-sub-background border',
-            'shadow-card dark:shadow-card-dark'
-          )}
-        >
-          {item} {index}
-        </div>
-      ))}
-    </div>
+    <section className="mt-5 w-full sm:mt-8">
+      <p className="text-2xs text-text/70 px-2 py-0.5 text-right sm:text-xs">
+        현재 <span className="font-medium">0</span> BINGO
+      </p>
+      <div className="mt-2 grid grid-cols-3 grid-rows-3 gap-2">
+        {array.map((item, index) => (
+          <div
+            key={index}
+            className={cn(
+              'flex aspect-3/4 items-center justify-center px-2 text-center text-xs font-medium break-keep sm:text-sm',
+              'border-text/25 bg-sub-background border',
+              'shadow-card dark:shadow-card-dark'
+            )}
+          >
+            {item} {index}
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
