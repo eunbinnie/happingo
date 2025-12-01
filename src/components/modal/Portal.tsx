cf34d958983b38fb6e130b477f759e8b55bf2203
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-12-01 20:47:51 +0900

    feat: 공통 모달 컴포넌트 추가 (#6)
    * feat: 공통 모달 컴포넌트 추가
    
    * feat: 모달 상태 관리 훅 추가
    
    * feat: 공통 모달 컴포넌트 적용 테스트

---
+++ src/components/modal/Portal.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { PropsWithChildren, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface PortalProps extends PropsWithChildren {
+  id?: string;
+}
+
+const Portal = ({ children, id = '__next-portal' }: PortalProps) => {
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const el = document.createElement('div');
+    el.id = id;
+    document.body.appendChild(el);
+    setContainer(el);
+
+    return () => {
+      el.remove();
+    };
+  }, [id]);
+
+  return container && createPortal(children, container);
+};
+
+export default Portal;
