b148c329b9406be8b81ad3beccb670e0d8b75f1b
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-12-02 16:30:30 +0900

    feat: 버튼 컴포넌트 추가 및 레이아웃 개선 (#7)
    * refactor: 빙고 편집 모달 제거
    
    * feat: 버튼 컴포넌트 추가
    
    * style: 빙고 편집 버튼 및 레이아웃 개선

--- src/components/bingo/BingoBoard.tsx
+++ src/components/bingo/BingoBoard.tsx
@@ -1,13 +1,27 @@
 import { cn } from '@/lib/cn';
 
+import Button from '../button/Button';
+
 const BingoBoard = () => {
   const array = new Array(9).fill('빙고');
 
   return (
     <section className="mt-5 w-full sm:mt-8">
-      <p className="text-2xs text-text/70 px-2 py-0.5 text-right sm:text-xs">
-        현재 <span className="font-medium">0</span> BINGO
-      </p>
+      <div className="flex items-center justify-between px-2 py-0.5">
+        <p className="text-2xs dark:text-text/60 sm:text-xs">
+          현재 <span className="font-medium">0</span> BINGO
+        </p>
+        <Button variant="outline" size="sm">
+          빙고 내용 편집
+        </Button>
+        {/* <div className="flex items-center gap-1 sm:gap-2">
+          <Button size="sm">저장하기</Button>
+          <Button variant="ghost" size="sm">
+            취소
+          </Button>
+        </div> */}
+      </div>
+
       <div className="mt-2 grid grid-cols-3 grid-rows-3 gap-2">
         {array.map((item, index) => (
           <div
--- src/components/layout/HeaderNav.tsx
+++ src/components/layout/HeaderNav.tsx
@@ -1,5 +1,5 @@
+import Button from '../button/Button';
 import ThemeChanger from '../button/ThemeChanger';
-import BingoEditModal from '../modal/BingoEditModal';
 
 const Separator = () => {
   return <div className="bg-text/60 h-[10px] w-px" />;
@@ -9,14 +9,12 @@ const HeaderNav = () => {
   return (
     <nav className="dark:text-text/60 mt-4 flex flex-wrap items-center justify-between gap-x-2 text-xs sm:mt-7 sm:text-sm">
       <div className="flex items-center gap-1.5">
-        <button type="button">지난 빙고</button>
+        <Button variant="ghost">지난 빙고</Button>
         <Separator />
-        <button type="button">이미지 저장</button>
+        <Button variant="ghost">이미지 저장</Button>
       </div>
       <div className="flex items-center gap-1.5">
-        <button type="button">해핑고 사용법</button>
-        <Separator />
-        <BingoEditModal />
+        <Button variant="ghost">해핑고 사용법</Button>
         <div className="flex items-center">
           <Separator />
           <ThemeChanger />
