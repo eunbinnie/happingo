869debb2ff9baa860f78ee34293a6da31ebc9d74
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-12-02 16:30:30 +0900

    feat: 버튼 컴포넌트 추가 및 레이아웃 개선 (#7)
    * refactor: 빙고 편집 모달 제거
    
    * feat: 버튼 컴포넌트 추가
    
    * style: 빙고 편집 버튼 및 레이아웃 개선

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,48 @@
+import React, { PropsWithChildren } from 'react';
+
+import { cn } from '@/lib/cn';
+
+type ButtonVariant = 'default' | 'outline' | 'ghost';
+type ButtonSize = 'sm' | 'md';
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    PropsWithChildren {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+}
+
+const base =
+  'dark:text-text/60 py-0.5 px-1 transition-colors rounded-md duration-300';
+
+const sizeClasses: Record<ButtonSize, string> = {
+  sm: 'text-2xs sm:text-xs',
+  md: 'text-xs sm:text-sm',
+};
+
+const variantClasses: Record<ButtonVariant, string> = {
+  default: 'bg-text/80 hover:bg-text text-background dark:text-background',
+  outline:
+    'hover:bg-sub-background border-text/25 border shadow-xs dark:hover:bg-gray-100/50',
+  ghost: 'hover:bg-gray-200 dark:hover:bg-gray-100/50',
+};
+
+const Button = ({
+  type = 'button',
+  children,
+  variant = 'default',
+  size = 'md',
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      {...props}
+      className={cn(base, sizeClasses[size], variantClasses[variant])}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
