2917e7ce91fb58cb9450a2ff64399b6518b6db2b
eunbinnie <dmsqlsdl54@gmail.com>
2025-12-09 15:56:04 +0900

    feat: 편집 모드에서 빙고 카드 앞/뒷면 전환 로직 추가
    - 편집 모드에서는 카드 뒤집기 불가
    - 편집 모드에서는 디폴트로 앞면만 보여주기

--- src/components/bingo/BingoCard.tsx
+++ src/components/bingo/BingoCard.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
-import { BingoItem } from '@/store';
+import { cn } from '@/lib/cn';
+import { BingoItem, useEditActionStore } from '@/store';
 
 import BackBingoCard from './BackBingoCard';
 import FrontBingoCard from './FrontBingoCard';
@@ -14,15 +15,37 @@ interface BingoCardProps {
 const BingoCard = ({ item }: BingoCardProps) => {
   const { id, text, image } = item;
   const cardRef = useRef<HTMLDivElement>(null);
+  const isEditing = useEditActionStore(state => state.isEditing);
 
   const handleCardClick = () => {
+    // 편집 모드이면 클릭 이벤트 무시
+    if (isEditing) return;
+
     cardRef.current?.classList.toggle('rotate-y-180');
   };
+
+  useEffect(() => {
+    // 현재 카드 뒷면을 보여주고 있는지
+    const isBackFace = cardRef.current?.classList.contains('rotate-y-180');
+
+    // 편집 모드이고 카드 뒷면을 보여주고 있으면 앞면으로 전환
+    if (isEditing && isBackFace) {
+      cardRef.current?.classList.remove('rotate-y-180');
+    }
+
+    // 편집 모드가 아니고 이미지가 있으면 뒷면으로 전환
+    if (!isEditing && image) {
+      cardRef.current?.classList.add('rotate-y-180');
+    }
+  }, [isEditing, image]);
 
   return (
     <div
       ref={cardRef}
-      className="inline-grid rotate-y-0 cursor-pointer transition-all duration-500 ease-in-out perspective-dramatic transform-3d"
+      className={cn(
+        'inline-grid rotate-y-0 transition-all duration-500 ease-in-out perspective-dramatic transform-3d',
+        !isEditing && 'cursor-pointer'
+      )}
       onClick={handleCardClick}
     >
       <FrontBingoCard text={text} />
