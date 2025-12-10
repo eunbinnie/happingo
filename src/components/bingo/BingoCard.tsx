f3e35dec712efeb4c233581928aedbf4bbada71c
eunbinnie <dmsqlsdl54@gmail.com>
2025-12-10 19:18:19 +0900

    refactor: 미사용 cardRef 제거

--- src/components/bingo/BingoCard.tsx
+++ src/components/bingo/BingoCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { cn } from '@/lib/cn';
 import { BingoItem, useEditActionStore } from '@/store';
@@ -15,8 +15,6 @@ interface BingoCardProps {
 
 const BingoCard = ({ item, firstBingoCardRef }: BingoCardProps) => {
   const { id, image } = item;
-  // TODO: cardRef 필요 여부 체크
-  const cardRef = useRef<HTMLDivElement | null>(null);
   const [isBack, setIsBack] = useState(false);
   const isEditing = useEditActionStore(state => state.isEditing);
 
@@ -35,7 +33,6 @@ const BingoCard = ({ item, firstBingoCardRef }: BingoCardProps) => {
 
   return (
     <div
-      ref={cardRef}
       className={cn(
         'inline-grid rotate-y-0 transition-all duration-500 ease-in-out perspective-dramatic transform-3d',
         isBack && 'rotate-y-180',
