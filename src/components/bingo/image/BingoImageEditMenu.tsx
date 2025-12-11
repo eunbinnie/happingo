c898456e330901f7f63e130f7959d542d051f3e9
eunbinnie <dmsqlsdl54@gmail.com>
2025-12-11 16:33:58 +0900

    refactor: 이미지 수정 아이콘 변경 및 카드 뒤집기 비활성화

--- src/components/bingo/image/BingoImageEditMenu.tsx
+++ src/components/bingo/image/BingoImageEditMenu.tsx
@@ -1,11 +1,18 @@
-import { Pencil } from 'lucide-react';
+import { EllipsisVertical } from 'lucide-react';
 
 const BingoImageEditMenu = () => {
+  const handleClick: React.MouseEventHandler<HTMLDivElement> = e => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className="absolute top-0 right-0 flex size-7 items-center justify-center p-2 md:size-8">
-      <Pencil
-        color="#f3f6f9"
-        className="drop-shadow-icon-soft hover:drop-shadow-icon-strong transition-all duration-300"
+    <div
+      onClick={handleClick}
+      className="absolute top-0 right-0 flex size-7 items-center justify-center p-2 md:size-8"
+    >
+      <EllipsisVertical
+        color="#fff"
+        className="drop-shadow-icon-strong transition-all duration-300"
       />
     </div>
   );
